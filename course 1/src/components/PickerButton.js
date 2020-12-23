6da37c8e97f871a650696bedcfdeb497017bf4b3
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2020-12-23 19:26:44 +0100

    refacto

--- course 1/src/components/PickerButton.js
+++ course 1/src/components/PickerButton.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const PickerButton = ({ str, color, setter }) => {
-    return (
-        <View>
-            <TouchableOpacity
-                style={[style.button, style.odd, {backgroundColor: str}]}
-                onPress={() => setter(color + 10)}
-            >
-                <Text>More {str}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={[style.button, style.even, {backgroundColor: str}]}
-                onPress={() => setter(color - 10)}
-            >
-                <Text>Less {str}</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+  return (
+    <View>
+      <TouchableOpacity
+        style={[style.button, style.odd, { backgroundColor: str }]}
+        onPress={() => (color + 10 > 255 ? null : setter(color + 10))}
+      >
+        <Text>More {str}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[style.button, style.even, { backgroundColor: str }]}
+        onPress={() => (color - 10 < 0 ? null : setter(color - 10))}
+      >
+        <Text>Less {str}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const style = StyleSheet.create({
-    button: {
-        color: "white",
-        padding: 15,
-    },
-    odd: {
-        marginTop: 20
-    },
-    even: {
-        marginBottom: 20
-    }
-})
+  button: {
+    color: "white",
+    padding: 15,
+  },
+  odd: {
+    marginTop: 20,
+  },
+  even: {
+    marginBottom: 20,
+  },
+});
 
 export default PickerButton;
