fd41c4371d3f76cdedd32400049a04114f50e639
Quentin Roux <quentin.roux@hotmail.fr>
2021-01-08 19:27:13 +0100

    authentication signup and signin + hashing + salting working

--- tracker-api/src/routes/authRoutes.js
+++ tracker-api/src/routes/authRoutes.js
@@ -7,7 +7,7 @@ import requireAuth from "../middlewares/requireAuth.js";
 const User = mongoose.model("User");
 const router = express.Router();
 
-router.post("/signup", requireAuth, async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = new User({ email, password });
@@ -18,6 +18,29 @@ router.post("/signup", requireAuth, async (req, res) => {
     res.send({ token });
   } catch (err) {
     res.status(422).send("Some error occured on SignupRoute:" + err.message);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
   }
 });
 
