b2d850dc0ff08f024b8cc69f27fd1043ba3eaa7d
Quentin Roux <quentin.roux@hotmail.fr>
2021-01-12 18:16:36 +0100

    location permission setup

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 export const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    points.push({
+      latitude: 43.2871991 + i * 0.001,
+      longitude: 5.3641216 + i * 0.001,
+    });
+  }
+
   return (
     <MapView
       style={style.mapContainer}
@@ -12,7 +20,9 @@ export const Map = () => {
         latitudeDelta: 0.5,
         longitudeDelta: 0.5,
       }}
-    />
+    >
+      <Polyline coordinates={points} />
+    </MapView>
   );
 };
 
--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { requestPermissionsAsync } from "expo-location";
 
 import { Map } from "../components/Map";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState("");
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error(
+          "Location permission not granted. Enable location to continue"
+        );
+      }
+    } catch (e) {
+      setErr(e.message);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView style={style.container}>
       <Text style={style.header}>New Track</Text>
       <Map />
+      {err ? (
+        <Text style={{ color: "red", fontWeight: "bold" }}>{err}</Text>
+      ) : null}
     </SafeAreaView>
   );
 };
